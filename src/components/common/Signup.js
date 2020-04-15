5f2c237beea137cbe38842e627fe401aca79bd31
Tyler Briskie <tylerbriskie@gmail.com>
2020-04-15 12:56:16 -0600

    successful login redirecting to empty settings page

--- src/components/common/Signup.js
+++ src/components/common/Signup.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
-import {TextField, Button} from '@material-ui/core';
+import { Redirect } from "react-router-dom";
+
+import {TextField, Button, Box} from '@material-ui/core';
+import { spacing } from '@material-ui/system';
+
+
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 // LOCAL FILES
 import "./Signup.css";
+
+
+const axios = require('axios');
+
 
 const SignupSchema = Yup.object().shape({
     email: Yup.string()
@@ -23,135 +32,194 @@ const SignupSchema = Yup.object().shape({
             "Must contain 8 characters at least one number, and one special case character"
           ),
     confirm_password: Yup.string()
-        .required()
+        .required('Passwords must match')
         .oneOf([Yup.ref("password"), null], "Passwords must match")
   });
 
 
-const Signup = () => {
+class Signup extends React.Component {
+
+        constructor(props){
+            super(props);
+            this.state = {
+                redirect: null,
+                error: {
+                    hasError: false, message: ''
+                }
+            }
+        }
 
 
 
 
+        render(){
+            if (this.state.redirect){
+                return <Redirect to={this.state.redirect} />
+            }
+            return ( 
+                <div className="page-container">
+                    <h2 className="page-section-header">SIGN UP</h2>
+                    <h4  className="page-section-header" >(We will never share your email address with anyone)</h4>
+                    <Formik
+                        initialValues={{            
+                            email: '',
+                            display_name: '',
+                            password: '',
+                            confirm_password: ''
+                        }} 
+                        validationSchema={SignupSchema}
+    
+                        onSubmit={(data, {setSubmitting, setErrors }) => {
+                            setSubmitting(true);
+    
+                            const postData = data;
+                            console.log(postData);
+    
+                            let currentComponent = this;
 
-
-
-
-        return ( 
-            <div>
-                <h2 className="page-section-header">SIGN UP</h2>
-                <h4  className="page-section-header" >(We will never share your email address with anyone)</h4>
-                <Formik
-                    initialValues={{            
-                        email: '',
-                        display_name: '',
-                        password: '',
-                        confirm_password: ''
-                    }} 
-                    validationSchema={SignupSchema}
-
-                    onSubmit={(data, {setSubmitting }) => {
-                        setSubmitting(true);
-
-                        // make async call
-                        setTimeout(() => {
-                            setSubmitting(false);
-                            alert('form submitted')
-                        }, 1000)
-
-                        console.log(data)
-                    }}   
-                        >
-                    {({ values, isSubmitting, handleChange, handleBlur, handleSubmit, isValid, errors, touched, dirty }) => (
-                        
-                        <Form id="new-user-form">
-                            <div>
-                                <div>
-                                    <Field 
-                                        placeholder="Email Address"
-                                        className="signup-form-input"
-                                        name="email" 
-                                        type="input" 
-                                        error={errors.email && touched.email}
-                                   
-                                        as={TextField}
-                                    />
-                                    {
-                                        errors.email && touched.email ? 
-                                        <div className="error-message">{errors.email}</div> : 
-                                        <div  className="error-message"></div>
-                                    }
-
-                                </div>
-                                <div>
-                                    <Field 
-                                        className="signup-form-input"
-                                        placeholder="Display name"
-                                        name="display_name" 
-                                        type="input"
-                                        error={errors.display_name && touched.display_name}
-                                        as={TextField}
-                                    />
-                                    {
-                                        errors.display_name && touched.display_name ? 
-                                        <div className="error-message">{errors.display_name}</div> : 
-                                        <div  className="error-message"></div>
-                                    }
-
-                                </div>
+                            axios.post(process.env.REACT_APP_API_BASE_URL+'auth/signup', postData)
+                              .then(function (response) {
+                                console.log('success', response);                                
+                                setSubmitting(false);
+                                currentComponent.setState({
+                                    redirect: '/settings'
+                                })
                                 
+                              })
+                              .catch(function (error) {
+                                console.log( error);
+                                if(error.response && error.response.status === 409){
+                                    currentComponent.setState({
+                                        error: {
+                                            hasError: true, message: error.response.data
+                                        }
+    
+                                    })
+                                    setErrors({
+                                        email: "a user with that email exists"
+                                    })
+                                }
+                         
+                                
+                                setSubmitting(false);
+                              });
+    
+    
+                            console.log(data)
+                        }}   
+                            >
+                        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit, isValid, errors, touched, dirty, handleReset }) => (
+                            
+                            <Form id="new-user-form">
                                 <div>
-                                    <Field 
-                                        className="signup-form-input"
-                                        placeholder="Password"
-                                        name="password" 
-                                        type="password"
-                                        error={errors.password && touched.password}
+                                    <div>
+                                        <Field 
+                                            placeholder="Email Address"
+                                            className="signup-form-input"
+                                            name="email" 
+                                            type="input" 
+                                            error={errors.email && touched.email}
+                                       
+                                            as={TextField}
+                                        />
+                                        {
+                                            errors.email && touched.email ? 
+                                            <div className="error-message">{errors.email}</div> : 
+                                            <div  className="error-message"></div>
+                                        }
+    
+                                    </div>
+                                    <div>
+                                        <Field 
+                                            className="signup-form-input"
+                                            placeholder="Display name"
+                                            name="display_name" 
+                                            type="input"
+                                            error={errors.display_name && touched.display_name}
+                                            as={TextField}
+                                        />
+                                        {
+                                            errors.display_name && touched.display_name ? 
+                                            <div className="error-message">{errors.display_name}</div> : 
+                                            <div  className="error-message"></div>
+                                        }
+    
+                                    </div>
+                                    
+                                    <div>
+                                        <Field 
+                                            className="signup-form-input"
+                                            placeholder="Password"
+                                            name="password" 
+                                            type="password"
+                                            error={errors.password && touched.password}
+    
+                                            as={TextField}
+                                        />
+                                        {
+                                            errors.password && touched.password ? 
+                                            <div className="error-message">{errors.password}</div> : 
+                                            <div  className="error-message"></div>
+                                        }
+    
+                                    </div>
+                                    <div>
+                                        <Field 
+                                            className="signup-form-input"
+                                            placeholder="Confirm Password"
+                                            name="confirm_password" 
+                                            type="password"
+                                            error={errors.confirm_password && touched.confirm_password}
+    
+                                            as={TextField}
+                                        />
+                                        {
+                                            errors.confirm_password && touched.confirm_password ? 
+                                            <div className="error-message">{errors.confirm_password}</div> : 
+                                            <div  className="error-message"></div>
+                                        }
+                                    </div>
+                                      
+                                </div>
+    
+                         
+    
+                                <div className="form-buttons-group">
+                                    <Button variant="contained" disabled={!dirty } type="reset">Clear</Button>
+    
+                                    <Button variant="contained" color="primary" disabled={isSubmitting || !isValid || !dirty } type="submit">Submit</Button>
+    
+                                </div>
+    
+                            </Form>
+                        )}
+                    </Formik>
+                    
+                        {
+                            this.state.error.hasError === true? 
+                            <Box mt={3}>
+                                <hr />
+                                
+                                <Box mt={3}>
+                                <h3>{this.state.error.message}</h3>
 
-                                        as={TextField}
-                                    />
-                                    {
-                                        errors.password && touched.password ? 
-                                        <div className="error-message">{errors.password}</div> : 
-                                        <div  className="error-message"></div>
-                                    }
+                                </Box>
+                                <div>
+                                    <Box mt={3}>
+                                        <Button variant="contained" color={"primary"} >Forgot Password</Button>
+
+                                    </Box>
 
                                 </div>
-                                <div>
-                                    <Field 
-                                        className="signup-form-input"
-                                        placeholder="Confirm Password"
-                                        name="confirm_password" 
-                                        type="password"
-                                        error={errors.confirm_password && touched.confirm_password}
+                            </Box> :
+                            null
+                        }                                        
+                    
+                
+                </div>
+             );
+        }
 
-                                        as={TextField}
-                                    />
-                                    {
-                                        errors.confirm_password && touched.confirm_password ? 
-                                        <div className="error-message">{errors.confirm_password}</div> : 
-                                        <div  className="error-message"></div>
-                                    }
-                                </div>
-                                  
-                            </div>
-                            <div>
-                          
-                            </div>
-
-                     
-
-                            <div>
-                                <Button disabled={isSubmitting || !isValid || !dirty } type="submit">Submit</Button>
-
-                            </div>
-
-                        </Form>
-                    )}
-                </Formik>
-
-            </div>
-         );
 
 }
  
