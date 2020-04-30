d788f82dd0f691807ce69c62424a744029aff48b
Tyler Briskie <tylerbriskie@gmail.com>
2020-04-30 09:58:26 -0600

    add player component

---
+++ src/components/common/player/Player.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Player.css';
+
+const Player = () => {
+    return ( 
+        <div className="individual-player-wrapper">
+            <h2>Player</h2>
+        </div>
+     );
+}
+ 
+export default Player;
