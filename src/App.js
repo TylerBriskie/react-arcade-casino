fad01994344be0863555ae386c846c2dc29dde8a
Tyler Briskie <tylerbriskie@gmail.com>
2020-05-05 14:37:43 -0600

    Replace homegrown themeContext with Material UI theme object

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Paper } from '@material-ui/core';
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 // LOCAL IMPORTS
+import Theme from './Themes';
 import NavBar from './components/common/NavBar';
 import Home from './components/common/Home';
 import Login from './components/common/Login';
 import Signup from './components/common/Signup';
 import './App.css';
-import ThemeContextProvider from './contexts/themeContext';
+// import ThemeContextProvider from './contexts/themeContext';
 import AuthContextProvider from './contexts/authContext';
 import AccountSettings from './components/common/AccountSettings';
 import CasinoHome from './components/casino/CasinoHome';
@@ -17,33 +22,39 @@ import BlackJack from './components/casino/blackjack/BlackJack';
 // MAIN APP
 function App() {
 
+  const theme = createMuiTheme(Theme)
+
+
   return (
     // <ThemeContextProvider>
-      <div className="App">
-        <ThemeContextProvider>
-          <AuthContextProvider>
-            <NavBar />
-            <Switch>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Paper className="App" style={{height: "100vh"}}>
 
-              <Route path="/" component={Home} exact />
-              <Route path="/account" component={AccountSettings} />
-              <Route path="/arcade" component={ ArcadeHome } />
-              <Route
-                path="/casino"
-                render={({ match: { url } }) => (
-                  <>
-                    <Route path={`${url}/`} component={CasinoHome} exact />
-                    <Route path={`${url}/blackjack`} component={BlackJack} />
-                  </>
-                )}
-              />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-            </Switch>
-          </AuthContextProvider>
-        </ThemeContextProvider>
+            <AuthContextProvider>
+              <NavBar />
+              <Switch>
+                
+                <Route path="/" component={Home} exact />
+                <Route path="/account" component={AccountSettings} />
+                <Route path="/arcade" component={ ArcadeHome } />
+                <Route
+                  path="/casino"
+                  render={({ match: { url } }) => (
+                    <>
+                      <Route path={`${url}/`} component={CasinoHome} exact />
+                      <Route path={`${url}/blackjack`} component={BlackJack} />
+                    </>
+                  )}
+                />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+              </Switch>
+            </AuthContextProvider>
+          </Paper>
 
-      </div>
+        </ThemeProvider>
+
     // </ThemeContextProvider>
 
 
---
+++ src/Themes.js
@@ -0,0 +1,20 @@
+
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import yellow from '@material-ui/core/colors/yellow';
+import blue from '@material-ui/core/colors/blue';
+
+
+const Theme = {
+    palette: {
+        type: 'dark',
+        primary: deepPurple,
+        secondary: yellow,
+        background: {
+            paper: "#000000",
+            default: '#222222',
+        },
+        
+    }
+  }
+  
+  export default Theme;
