0464f0e9725fa2827e0f252f7128aebf9748aa8d
Tyler Briskie <tylerbriskie@gmail.com>
2020-05-05 11:10:34 -0600

    create player component, bring into blackjack game container

--- src/components/casino/blackjack/BlackJack.js
+++ src/components/casino/blackjack/BlackJack.js
@@ -6,12 +6,61 @@ import Player from '../../common/player/Player';
 import './BlackJack.css';
 
 
+const PLACE_YOUR_BETS = "PLACE_YOUR_BETS";
+const PLAYER_TURNS = "PLAYER_TURNS";
+const DEALER_TURNS = "DEALER_TURNS";
+const PAYOUT = "PAYOUT";
+
 
 class BlackJack extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            players: [{
+                seat: 1,
+                name: 'Player One',
+                credits: 5000,
+                cards: [],
+                value: 0,
+                hasBlackjack: false,
+            }, {
+                seat: 2,
+                name: 'Player Two',
+                credits: 5000,
+                cards: [],
+                value: 0,
+                hasBlackjack: true,
+            }, {
+                seat: 3,
+                name: 'Mickey Mouse',
+                credits: 100,
+                cards: [],
+                value: 22,
+                hasBlackjack: false,
+            }],
+            dealer: {
+                seat: 5,
+                cards: [],
+                value: 0,
+                isBlackjack: false,
+            },
+            playerTurn: 1,
+            gamePhase: PLACE_YOUR_BETS,
+
+        }
     }
 
+
+    changeGamePhase = async (e) => {
+        console.log('changing game phase, ', e.target.value);
+
+        await this.setState({
+            gamePhase: e.target.value
+        })
+
+        console.log(this.state);
+
+    }
 
     render(){
         return (
@@ -20,12 +69,13 @@ class BlackJack extends Component {
                     BLACKJACK GAME
                 </h2>
                 <div className="players-container">
-                <Player />
-                <Player />
-
-                <Player />
-
-                <Player />
+                {this.state.players.map(p => 
+                    <Player 
+                        details={{...p}} 
+                        isPlayerTurn={this.state.playerTurn === p.seat}
+                        changeGamePhase={() => this.changeGamePhase}
+                    />
+                )}
 
                 </div>
 
--- src/components/common/player/Player.js
+++ src/components/common/player/Player.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import Fab from '@material-ui/core/Fab';
+
+// LOCAL
 import './Player.css';
 
-const Player = () => {
+const Player = props => {
+
+    const renderButtons = () => {
+
+        const buttons = [];
+
+        if(props.details.hasBlackjack){
+            console.log('player ' + props.details.name + ' has blackjack');
+            return (
+                <div className="player-actions">
+                    <h3>Blackjack!</h3>
+                </div>
+            )
+        }
+
+        if (props.details.value > 21){
+            console.log('player ' + props.details.seat + ' busts');
+            return (
+                <div className="player-actions">
+                    <h3>Bust!</h3>
+                </div>
+            )
+        }
+
+
+
+        return (
+            <div className="player-actions">
+                {
+                   
+                }
+                <Fab color="primary" aria-label="hit" onClick={props.changeGamePhase("PAYOUT")}>
+                    Hit
+                </Fab>
+                <Fab color="primary" aria-label="stay">
+                    Stay
+                </Fab>
+            </div>
+        )
+    }
+
     return ( 
         <div className="individual-player-wrapper">
-            <h2>Player</h2>
+            <div className="player-details">
+
+            </div>
+            <h2>{props.details.name}</h2>
+            <div className="cards-container">
+                CARDS
+            </div>
+                {renderButtons()}
+                
         </div>
      );
 }
